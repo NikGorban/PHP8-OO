cedad9e07848b72ccb0ba1735b8f753644aa040b
Mykyta Horban <masarash12345@gmail.com>
2025-10-03 09:13:02 +0200

    merge ex6

--- classe2/mykyta/bases-oo-RPG/06-namespace/controller/routerController.php
+++ classe2/mykyta/bases-oo-RPG/06-namespace/controller/routerController.php
@@ -1,8 +1,10 @@
 <?php
 // echo __FILE__; // chemin et nom de fichier
 
-// chemin vers la classe
+// chemin vers la classe Manager d'Article
 use model\ArticleManager;
+// chemin vers le mapping d'Article
+use model\ArticleMapping;
 
 # Connexion PDO
 try {
@@ -32,6 +34,58 @@ if(isset($_GET['p'])){
         case 'admin':
             $nosArticle = $ArticleManager->readAll();
             include RACINE_PATH . "/view/admin.html.php";
+            break;
+        case 'create':
+            // si le formulaire est envoyé
+            if(isset($_POST['article_title'],$_POST['article_text'],$_POST['article_visibility'])){
+                try {
+                    // utilisation des setters
+                    $newArticle = new ArticleMapping($_POST);
+                    // pas de faute création du slug
+                    // qui est importé par 'use SlugifyTrait'
+                    // dans ArticleManager, on décode l'htmspecialchars
+                    // pour éviter des caractères parasites dans le nom
+                    // du slug
+                    $slug = $ArticleManager->slugify(html_entity_decode($newArticle->getArticleTitle()));
+                    // on utilise le setter d'Article pour mettre
+                    // à jour article_slug
+                    $newArticle->setArticleSlug($slug);
+
+                    // on veut insérer l'article
+                    $ok = $ArticleManager->create($newArticle);
+                    if($ok === true){
+                        // redirection vers la page d'admin
+                        header("Location: ./?p=admin");
+                        exit;
+                    }else{
+                        // erreur lors de l'insertion
+                        $message = $ok;
+                    }
+
+                }catch (Exception $e){
+                    die($e->getMessage());
+                }
+            }
+                include RACINE_PATH . "/view/create.html.php";
+
+            break;
+        case 'update':
+                if(!empty($_GET['id'])  && ctype_digit($_GET['id'])):
+                    // récupération de l'article
+                    $article = $ArticleManager->readById((int)$_GET['id']);
+                    include RACINE_PATH."/view/update.html.php";
+                else:
+                    $message = "Touche pas à mon code !";
+                    include RACINE_PATH."/view/404.html.php";
+                endif;
+            break;
+        case 'delete':
+            if(!empty($_GET['id'])  && ctype_digit($_GET['id'])):
+
+            else:
+                $message = "Touche pas à mon code !";
+                include RACINE_PATH."/view/404.html.php";
+            endif;
             break;
     }
 
--- classe2/mykyta/bases-oo-RPG/06-namespace/model/ArticleManager.php
+++ classe2/mykyta/bases-oo-RPG/06-namespace/model/ArticleManager.php
@@ -3,41 +3,49 @@
 namespace model;
 
 use PDO;
+use Exception;
 
+// implémentation de 2 interfaces
 class ArticleManager implements ManagerInterface, CrudInterface
 {
     private PDO $db;
+
 
     // implémenté à cause de MangerInterface
     public function __construct(PDO $connect){
         $this->db = $connect;
     }
 
+    // Appel du Trait pour slugifier le titre
+    use SlugifyTrait;
+
     /*
      * méthodes implémentées à cause de CrudInterface
+     * AbstractMapping est le parent de tous nos
+     * mapping, c'est-à-dire données où on applique un CRUD
+     * dans notre cas une bdd MySQL
      */
-    public function create(AbstractMapping $data)
+    public function create(AbstractMapping $data): bool|string
     {
-        // TODO: Implement create() method.
-        $sql = "INSERT INTO article (article_title, article_slug, article_text, article_date, article_visibility)
-            VALUES (?,?,?,?,?);";
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute([
-            $data->getArticleTitle(),
-            $data->slugify($data->getArticleTitle()),
-            $data->getArticleText(),
-            $data->getArticleDate(),
-            $data->getArticleVisibility()
-        ]);
+        $sql = "INSERT INTO `article` (`article_title`, `article_slug`, `article_text`, `article_visibility`) VALUES (?,?,?,?)";
+        $prepare = $this->db->prepare($sql);
+        try {
+            $prepare->execute([
+                $data->getArticleTitle(),
+                $data->getArticleSlug(),
+                $data->getArticleText(),
+                $data->getArticleVisibility()
+            ]);
+            return true;
+        }catch (Exception $e){
+            return $e->getMessage();
+        }
+
     }
 
     public function readById(int $id): bool|AbstractMapping
     {
-        // TODO: Implement readById() method.
-        $sql = "SELECT * FROM `article` WHERE `id`=:id";
-        $query = $this->db->prepare($sql);
-        $query->bindValue(':id',$id);
-        $query->execute();
+        return false;
     }
 
     // récupération de tous nos articles
@@ -58,24 +66,12 @@ class ArticleManager implements ManagerInterface, CrudInterface
 
     public function update(int $id, AbstractMapping $data)
     {
-        $sql = "UPDATE `article` SET article_title = ?, article_slug = ?, article_text = ?, article_date = ?, article_visibility = ? WHERE id = ?";
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute([
-            $data->getArticleTitle(),
-            $data->slugify($data->getArticleTitle()),
-            $data->getArticleText(),
-            $data->getArticleDate(),
-            $data->getArticleVisibility(), 
-            $id
-            ]);
-        }
+        // TODO: Implement update() method.
+    }
 
     public function delete(int $id)
     {
         // TODO: Implement delete() method.
-        $sql = "DELETE FROM `article` WHERE id=?";
-        $query = $this->db->prepare($sql);        
-        $query->execute([$id]);
     }
 
     /*
@@ -83,6 +79,7 @@ class ArticleManager implements ManagerInterface, CrudInterface
      */
 
     // on souhaite ne récupérer que les articles visibles
+    // pour la page d'accueil
     public function readAllVisible(bool $orderDesc = true): array
     {
         $sql = "SELECT * FROM `article` WHERE `article_visibility`=1 ";
